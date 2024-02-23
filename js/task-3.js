cf3642d0e5855b324078c24064d6bd65745ac20e
sssBu4 <skleto2004denis@gmail.com>
2024-02-23 02:17:27 +0200

    js up

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+    const totalSize = parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
+    return totalSize;
+}
+
+getElementWidth ()
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
